c973e69b8c00f4e7a9124afc63451b735a3a8b7f
dejetem <enyeoluwarotimisimon@gmail.com>
2023-02-27 11:19:45 +0100

    unit test update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -106,6 +106,15 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         self.assertEqual(testCls.repos_payload, self.repos_payload)
         self.assertEqual(testCls.public_repos(), self.expected_repos)
         self.mock.assert_called()
+    
+    def test_public_repos_with_license(self) -> None:
+        """
+        tests public repos method with a license.
+        """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
 
     @classmethod
     def tearDownClass(cls):
